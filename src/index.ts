0edddae825607675c180fcd0d36258dbd9267479
Joe Burton <joeburton@gmail.com>
2024-05-03 12:04:09 +0100

    pirate data

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,133 @@ app.get("/api/item/:slug", (req: Request, res: Response) => {
   res.end(`Item: ${slug}`);
 });
 
+app.get("/pirates", async (req, res) => {
+  try {
+    const users = {
+      rows: [
+        {
+          id: "234234",
+          name: "Captain Hook",
+          email: "captainhook@hook.com",
+        },
+        {
+          id: "345345",
+          name: "Blackbeard",
+          email: "blackbeard@pirates.com",
+        },
+        {
+          id: "456456",
+          name: "William Kidd",
+          email: "williamkidd@pirates.com",
+        },
+        {
+          id: "567567",
+          name: "Bartholomew Roberts",
+          email: "bartholomewroberts@pirates.com",
+        },
+        {
+          id: "678678",
+          name: "Anne Bonny",
+          email: "annebonny@pirates.com",
+        },
+        {
+          id: "789789",
+          name: "Edward Teach",
+          email: "edwardteach@pirates.com",
+        },
+        {
+          id: "890890",
+          name: "Calico Jack",
+          email: "calicojack@pirates.com",
+        },
+        {
+          id: "901901",
+          name: "Henry Morgan",
+          email: "henrymorgan@pirates.com",
+        },
+        {
+          id: "012012",
+          name: "Francis Drake",
+          email: "francisdrake@pirates.com",
+        },
+        {
+          id: "123123",
+          name: "Samuel Bellamy",
+          email: "samuelbellamy@pirates.com",
+        },
+      ],
+    };
+
+    if (users && users.rows.length > 0) {
+      let tableContent = users.rows
+        .map(
+          (user) =>
+            `<tr>
+                        <td>${user.id}</td>
+                        <td>${user.name}</td>
+                        <td>${user.email}</td>
+                    </tr>`
+        )
+        .join("");
+
+      res.status(200).send(`
+                <html>
+                    <head>
+                        <title>Users</title>
+                        <style>
+                            body {
+                                font-family: Arial, sans-serif;
+                            }
+                            table {
+                                width: 100%;
+                                border-collapse: collapse;
+                                margin-bottom: 15px;
+                            }
+                            th, td {
+                                border: 1px solid #ddd;
+                                padding: 8px;
+                                text-align: left;
+                            }
+                            th {
+                                background-color: #f2f2f2;
+                            }
+                            a {
+                                text-decoration: none;
+                                color: #0a16f7;
+                                margin: 15px;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <h1>Users</h1>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>User ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                ${tableContent}
+                            </tbody>
+                        </table>
+                        <div>
+                            <a href="/">Home</a>
+                            <a href="/uploadUser">Add User</a>
+                        </div>
+                    </body>
+                </html>
+            `);
+    } else {
+      res.status(404).send("Users not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving users");
+  }
+});
+
 app.listen(port, () => {
   return console.log(`Server is listening on ${port}`);
 });
