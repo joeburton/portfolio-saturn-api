771b82bbaff03a653b8878e122c70fd826a52ea8
Joe Burton <joeburton@gmail.com>
2024-05-03 10:14:58 +0100

    updating vercel config

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from "express";
+import { v4 } from "uuid";
 
-const app = express()
-const port = process.env.PORT || 8080
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.get('/', (_req: Request, res: Response) => {
-	return res.send('Express Typescript on Vercel')
-})
+app.get("/", (_req: Request, res: Response) => {
+  return res.send("Express Typescript on Vercel");
+});
 
-app.get('/ping', (_req: Request, res: Response) => {
-	return res.send('pong 🏓')
-})
+app.get("/ping", (_req: Request, res: Response) => {
+  return res.send("pong 🏓");
+});
+
+app.get("/api", (_req: Request, res: Response) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get("/api/item/:slug", (req: Request, res: Response) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
 
 app.listen(port, () => {
-	return console.log(`Server is listening on ${port}`)
-})
+  return console.log(`Server is listening on ${port}`);
+});
