7f4e2c34f2a823108490c542b3c0f63d8f06c0fe
Joe Burton <joeburton@gmail.com>
2024-05-03 12:14:10 +0100

    pirate data - updated res.send to res.end

--- src/index.ts
+++ src/index.ts
@@ -24,75 +24,77 @@ app.get("/api/item/:slug", (req: Request, res: Response) => {
   res.end(`Item: ${slug}`);
 });
 
-app.get("/pirates", async (req, res) => {
-  const pirates = {
-    rows: [
-      {
-        id: "234234",
-        name: "Captain Hook",
-        email: "captainhook@hook.com",
-      },
-      {
-        id: "345345",
-        name: "Blackbeard",
-        email: "blackbeard@pirates.com",
-      },
-      {
-        id: "456456",
-        name: "William Kidd",
-        email: "williamkidd@pirates.com",
-      },
-      {
-        id: "567567",
-        name: "Bartholomew Roberts",
-        email: "bartholomewroberts@pirates.com",
-      },
-      {
-        id: "678678",
-        name: "Anne Bonny",
-        email: "annebonny@pirates.com",
-      },
-      {
-        id: "789789",
-        name: "Edward Teach",
-        email: "edwardteach@pirates.com",
-      },
-      {
-        id: "890890",
-        name: "Calico Jack",
-        email: "calicojack@pirates.com",
-      },
-      {
-        id: "901901",
-        name: "Henry Morgan",
-        email: "henrymorgan@pirates.com",
-      },
-      {
-        id: "012012",
-        name: "Francis Drake",
-        email: "francisdrake@pirates.com",
-      },
-      {
-        id: "123123",
-        name: "Samuel Bellamy",
-        email: "samuelbellamy@pirates.com",
-      },
-    ],
-  };
+app.get("/pirates", async (_req: Request, res: Response) => {
+  try {
+    const pirates = {
+      rows: [
+        {
+          id: "234234",
+          name: "Captain Hook",
+          email: "captainhook@hook.com",
+        },
+        {
+          id: "345345",
+          name: "Blackbeard",
+          email: "blackbeard@pirates.com",
+        },
+        {
+          id: "456456",
+          name: "William Kidd",
+          email: "williamkidd@pirates.com",
+        },
+        {
+          id: "567567",
+          name: "Bartholomew Roberts",
+          email: "bartholomewroberts@pirates.com",
+        },
+        {
+          id: "678678",
+          name: "Anne Bonny",
+          email: "annebonny@pirates.com",
+        },
+        {
+          id: "789789",
+          name: "Edward Teach",
+          email: "edwardteach@pirates.com",
+        },
+        {
+          id: "890890",
+          name: "Calico Jack",
+          email: "calicojack@pirates.com",
+        },
+        {
+          id: "901901",
+          name: "Henry Morgan",
+          email: "henrymorgan@pirates.com",
+        },
+        {
+          id: "012012",
+          name: "Francis Drake",
+          email: "francisdrake@pirates.com",
+        },
+        {
+          id: "123123",
+          name: "Samuel Bellamy",
+          email: "samuelbellamy@pirates.com",
+        },
+      ],
+    };
 
-  let tableContent = pirates.rows
-    .map(
-      (user) =>
-        `<tr>
+    if (pirates && pirates.rows.length > 0) {
+      let tableContent = pirates.rows
+        .map(
+          (user) =>
+            `<tr>
                 <td>${user.id}</td>
                 <td>${user.name}</td>
                 <td>${user.email}</td>
             </tr>`
-    )
-    .join("");
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.status(200).send(`
+        )
+        .join("");
+      res.setHeader("Content-Type", "text/html");
+      res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+      res.end(`
                 <html>
                     <head>
                         <title>Pirates</title>
@@ -140,6 +142,13 @@ app.get("/pirates", async (req, res) => {
                     </body>
                 </html>
             `);
+    } else {
+      res.status(404).send("Pirates not found");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving pirates");
+  }
 });
 
 app.listen(port, () => {
